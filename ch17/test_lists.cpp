366f9a7729df1d32dec4d3199c6e9c4885e28c32
IsaacStudley <studleyisaac@gmail.com>
2025-02-05 00:38:17 -0500

    ch17

---
+++ ch17/test_lists.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#include <string>
+#include "LinkedList.h"
+using namespace std;
+
+TEST_CASE("Test can create and render List Nodes") {
+    Node* node1 = new Node;
+    CHECK(node1->cargo == 0);
+}
+
+TEST_CASE("Test can create and render List Nodes") {
+    Node* node1 = new Node;
+    CHECK(node1->cargo == 0);
+    Node* node2 = new Node(42);
+    CHECK(node2->cargo == 42);
+    CHECK(node1->to_string() == "0");
+    CHECK(node2->to_string() == "42");
+}
