8aee872a7d675bacb161a3c0c087f33fe254f349
IsaacStudley <studleyisaac@gmail.com>
2025-02-11 08:54:24 -0500

    doublelinked

---
+++ ch17/src/DoubleLinkedList.cpp
@@ -0,0 +1,59 @@
+#include "DoubleLinkedList.h"
+using namespace std;
+#include <string>
+
+Node::Node()
+{
+    cargo = 0;
+    next = nullptr;
+}
+
+Node::Node(int cargo)
+{
+    this->cargo = cargo;
+    next = nullptr;
+}
+
+Node::Node(int cargo, Node* next)
+{
+    this->cargo = cargo;
+    this->next = next;
+}
+
+string Node::to_string() {
+    return std::to_string(cargo);
+}
+
+string render_list(Node* node) {
+    string result = node->to_string();
+    while (node->next != nullptr) {
+        node = node->next;
+        result += (", " + node->to_string());
+    }
+    return result;
+}
+
+string render_list_backward(Node* node) {
+    string result = node->to_string();
+    while (node->next != nullptr) {
+        node = node->next;
+        result = (node->to_string() + ", " + result);
+    }
+    return result;
+}
+
+string render_pretty(Node* node, string(*ren)(Node*)) {
+    return "("+ ren(node) +")";
+}
+
+Node* remove_second(Node* list) {
+    Node* first = list;
+    Node* second = list->next;
+
+    // make the first node point to the third
+    first->next = second->next;
+
+    // remove the second node from the list and return a pointer to it
+    second->next = nullptr;
+    return second;
+}
---
+++ ch17/src/DoubleLinkedList.h
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+
+struct Node {
+    int cargo;
+    Node* next;
+    Node* prev;
+    // constructors
+    Node();
+    Node(int);
+    Node(int, Node*);
+
+    // member functions
+    string to_string();
+};
+    string render_list(Node*);
+    string render_list_backward(Node*);
+    string render_pretty(Node*, string(*ren)(Node*));
+
+    Node* remove_second(Node*);
